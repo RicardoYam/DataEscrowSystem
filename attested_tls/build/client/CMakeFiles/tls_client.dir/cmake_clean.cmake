e42a6662a9d31173e9e5dab3a4a7bde2b3769622
Ubuntu <azureuser@vm-thesis.c1dbrg2vr1ju5ny4ceygmul3hc.px.internal.cloudapp.net>
2024-06-03 03:57:20 +0000

    Project finished

--- attested_tls/build/client/CMakeFiles/tls_client.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tls_client"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tls_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- attested_tls/build/client/enc/CMakeFiles/tls_client_sign_enc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tls_client_sign_enc"
-  "tls_client_enc.signed"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tls_client_sign_enc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
